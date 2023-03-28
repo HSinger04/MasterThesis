d6aca66c6abffbd3a82a07da493fa4e2aa8dea70
HSinger <hsinger@uos.de>
2023-03-28 15:58:53 +0200

    Prepare train_configs.sh for hd_gcn

--- graph_metric_learning-master/train_configs.sh
+++ graph_metric_learning-master/train_configs.sh
@@ -1,2 +1,7 @@
-python3 train.py --config-name train_sttformer_joint_motion_with_val +mode.type=train_from_scratch +mode.model_folder=./outputs/train_sttformer_joint_motion_with_val/example_saved_models
-python3 train.py --config-name train_sttformer_bone_with_val +mode.type=train_from_scratch +mode.model_folder=./outputs/train_sttformer_joint_motion_with_val/example_saved_models
+python3 train.py --config-name train_sttformer_joint_motion_no_val +mode.type=train_from_scratch +trainer.save_epochs=[26]
+python3 train.py --config-name train_hd_gcn_bone_com_1_no_val +mode.type=train_from_scratch +trainer.save_epochs=[44]
+python3 train.py --config-name train_hd_gcn_bone_com_2_with_val +mode.type=train_from_scratch +trainer.save_epochs=[]
+python3 train.py --config-name train_hd_gcn_bone_com_21_with_val +mode.type=train_from_scratch +trainer.save_epochs=[]
+python3 train.py --config-name train_hd_gcn_joint_com_1_with_val +mode.type=train_from_scratch +trainer.save_epochs=[]
+python3 train.py --config-name train_hd_gcn_joint_com_2_with_val +mode.type=train_from_scratch +trainer.save_epochs=[]
+python3 train.py --config-name train_hd_gcn_joint_com_21_with_val +mode.type=train_from_scratch +trainer.save_epochs=[]
