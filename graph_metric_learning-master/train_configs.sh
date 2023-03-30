fb216d2d339df9a86d5059dfe022293a0e4ae9cf
HSinger <hsinger@uos.de>
2023-03-30 11:28:15 +0200

    Fix configs for hd-gcn

--- graph_metric_learning-master/train_configs.sh
+++ graph_metric_learning-master/train_configs.sh
@@ -1,7 +1,3 @@
-python3 train.py --config-name train_sttformer_joint_motion_no_val +mode.type=train_from_scratch +trainer.save_epochs=[26]
-python3 train.py --config-name train_hd_gcn_bone_com_1_no_val +mode.type=train_from_scratch +trainer.save_epochs=[44]
-python3 train.py --config-name train_hd_gcn_bone_com_2_with_val +mode.type=train_from_scratch +trainer.save_epochs=[]
-python3 train.py --config-name train_hd_gcn_bone_com_21_with_val +mode.type=train_from_scratch +trainer.save_epochs=[]
 python3 train.py --config-name train_hd_gcn_joint_com_1_with_val +mode.type=train_from_scratch +trainer.save_epochs=[]
 python3 train.py --config-name train_hd_gcn_joint_com_2_with_val +mode.type=train_from_scratch +trainer.save_epochs=[]
 python3 train.py --config-name train_hd_gcn_joint_com_21_with_val +mode.type=train_from_scratch +trainer.save_epochs=[]
