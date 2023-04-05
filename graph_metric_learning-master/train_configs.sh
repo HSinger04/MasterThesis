ed7966222905a06b92f0150a668990c3f9895448
HSinger <hsinger@uos.de>
2023-04-05 10:30:02 +0200

    Incomplete draft for train_configs.sh

--- graph_metric_learning-master/train_configs.sh
+++ graph_metric_learning-master/train_configs.sh
@@ -1,3 +1,12 @@
-python3 train.py --config-name train_hd_gcn_joint_com_1_with_val +mode.type=train_from_scratch +trainer.save_epochs=[]
-python3 train.py --config-name train_hd_gcn_joint_com_2_with_val +mode.type=train_from_scratch +trainer.save_epochs=[]
-python3 train.py --config-name train_hd_gcn_joint_com_21_with_val +mode.type=train_from_scratch +trainer.save_epochs=[]
+python3 train.py --config-name train_sttformer_bone_with_val +mode.type=train_from_scratch +trainer.save_epochs=[]
+python3 train.py --config-name train_sttformer_joint_motion_with_val +mode.type=train_from_scratch +trainer.save_epochs=[]
+
+python3 train.py --config-name train_sttformer_joint_no_val +mode.type=train_from_scratch +trainer.save_epochs=[33]
+
+# TODO
+python3 train.py --config-name train_hd_gcn_joint_com_1_no_val +mode.type=train_from_scratch +trainer.save_epochs=[?]
+python3 train.py --config-name train_hd_gcn_joint_com_2_no_val +mode.type=train_from_scratch +trainer.save_epochs=[?]
+python3 train.py --config-name train_hd_gcn_joint_com_21_no_val +mode.type=train_from_scratch +trainer.save_epochs=[?]
+
+python3 train.py --config-name train_hd_gcn_bone_com_2_no_val +mode.type=train_from_scratch +trainer.save_epochs=[37]
+python3 train.py --config-name train_hd_gcn_bone_com_21_no_val +mode.type=train_from_scratch +trainer.save_epochs=[24]
