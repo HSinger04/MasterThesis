27e63d0c9101942911975a8d41a8765abeec9530
HSinger04 <hermannsinger@gmx.net>
2023-04-18 11:24:48 +0200

    Update train_configs.sh

--- graph_metric_learning-master/train_configs.sh
+++ graph_metric_learning-master/train_configs.sh
@@ -1,8 +1,10 @@
+python3 train.py --config-name train_hyperformer_bone_vel_with_val +mode.type=train_from_scratch +trainer.save_epochs=[]
+# python3 train.py --config-name train_hyperformer_bone_with_val +mode.type=train_from_scratch +trainer.save_epochs=[]
+# python3 train.py --config-name train_hyperformer_joint_vel_with_val +mode.type=train_from_scratch +trainer.save_epochs=[]
+# python3 train.py --config-name train_hyperformer_joint_with_val +mode.type=train_from_scratch +trainer.save_epochs=[]
+
 python3 train.py --config-name train_sttformer_joint_no_val +mode.type=train_from_scratch +trainer.save_epochs=[49]
 python3 train.py --config-name train_sttformer_bone_no_val +mode.type=train_from_scratch +trainer.save_epochs=[23]
 python3 train.py --config-name train_sttformer_joint_motion_no_val +mode.type=train_from_scratch +trainer.save_epochs=[45]
 
-# python3 train.py --config-name train_hyperformer_bone_vel_with_val +mode.type=train_from_scratch +trainer.save_epochs=[]
-# python3 train.py --config-name train_hyperformer_bone_with_val +mode.type=train_from_scratch +trainer.save_epochs=[]
-# python3 train.py --config-name train_hyperformer_joint_vel_with_val +mode.type=train_from_scratch +trainer.save_epochs=[]
-# python3 train.py --config-name train_hyperformer_joint_with_val +mode.type=train_from_scratch +trainer.save_epochs=[]
+
